9a66ac410a916c7d3623e74854f1841d14308569
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-12-02 15:20:51 -0300

    feat: use middleware when has authentication

--- src/lib/router/index.ts
+++ src/lib/router/index.ts
@@ -1,13 +1,13 @@
 import { errorDispatcher } from '~/middleware/error-dispatcher'
 import { Router as ExpressRouter } from 'express'
+import { authenticate } from '~/middleware/authenticate'
 
 export default function router(routes: AppRouter.Routes) {
   const router = ExpressRouter()
 
   for (const route of routes) {
     if (route?.authenticated) {
-      // create the authenticate middleware
-      // routes[route.method](route.path, authenticate, errorDispatcher(route.handler))
+      router[route.method](route.path, authenticate, errorDispatcher(route.handler))
     } else {
       router[route.method](route.path, errorDispatcher(route.handler))
     }
